1242d61e8b20119340d8cc027b76291e00ab4320
Дзибук Александр <124492308+dzibukalexander@users.noreply.github.com>
2023-10-07 11:28:41 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,22 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
 
-void main() {
-  std::cout<< "text";
+int main() 
+{
+    int exponent;
+    float base, result = 1;
+
+    cout << "Enter base and exponent respectively:  ";
+    cin >> base >> exponent;
+
+    cout << base << "^" << exponent << " = ";
+
+    while (exponent != 0) {
+        result *= base;
+        --exponent;
+    }
+
+    cout << result;
+    
+    return 0;
 }
