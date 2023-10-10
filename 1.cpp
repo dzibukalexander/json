3796072eb823eed01dbecd989fe7b1d76d6722ab
Дзибук Александр <124492308+dzibukalexander@users.noreply.github.com>
2023-10-10 13:56:11 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -8,7 +8,6 @@ int main()
     cout << "Enter base and exponent respectiy:  ";
     cin >> base >> exponent;
     cout << base << "^" << exponent << " = ";
-
     while (exponent != 0) {
         result *= base;
         --exponent;
